a10ad429249fda1ad421c40b67953595d77d43b0
kush2863 <70647570+kush2863@users.noreply.github.com>
2024-07-27 19:26:32 +0530

    Done

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -1,24 +1,25 @@
-'use client'
- import { ClientSideSuspense, RoomProvider } from '@liveblocks/react/suspense'
-import React, { use, useEffect, useRef, useState } from 'react'
-import Header from '@/components/Header'
-import { Editor } from '@/components/editor/Editor'
-import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
-import ActiveCollaborators from './ActiveCollaborators'
-import { Input } from './ui/input'
-import Image from 'next/image'
-import { updateDocument } from '@/lib/actions/room.actions'
-import Loader from './Loader'
-import ShareModel from './ShareModel'
+'use client';
 
- const CollaborativeRoom = ({roomId , roomMetadata, users, currentUserType}:CollaborativeRoomProps) => {
-  
-  const [documentTitle, useDocumentTitle] = useState(roomMetadata.title); 
+import { ClientSideSuspense, RoomProvider } from '@liveblocks/react/suspense'
+import { Editor } from '@/components/editor/Editor'
+import Header from '@/components/Header'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import ActiveCollaborators from './ActiveCollaborators';
+import { useEffect, useRef, useState } from 'react';
+import { Input } from './ui/input';
+import Image from 'next/image';
+import { updateDocument } from '@/lib/actions/room.actions';
+import Loader from './Loader';
+import ShareModal from './ShareModel';
+
+const CollaborativeRoom = ({ roomId, roomMetadata, users, currentUserType }: CollaborativeRoomProps) => {
+  const [documentTitle, setDocumentTitle] = useState(roomMetadata.title);
   const [editing, setEditing] = useState(false);
   const [loading, setLoading] = useState(false);
 
   const containerRef = useRef<HTMLDivElement>(null);
   const inputRef = useRef<HTMLDivElement>(null);
+
   const updateTitleHandler = async (e: React.KeyboardEvent<HTMLInputElement>) => {
     if(e.key === 'Enter') {
       setLoading(true);
@@ -60,77 +61,70 @@ import ShareModel from './ShareModel'
     }
   }, [editing])
   
-  
-   return (
-     <div> 
-         <RoomProvider id={roomId}>
-     <ClientSideSuspense fallback={<Loader />}>
+
+  return (
+    <RoomProvider id={roomId}>
+      <ClientSideSuspense fallback={<Loader />}>
         <div className="collaborative-room">
-        <Header>
-        <div ref={containerRef} className="flex w-fit items-center justify-center gap-2">
-         {editing && !loading?(
-          <Input 
-          type='text'
-          value={documentTitle}
-          ref={inputRef}
-          placeholder='enter'
-          onChange={(e) => useDocumentTitle(e.target.value)}
-          onKeyDown={updateTitleHandler }
-          disabled={!editing}
-          className='document-title-input'
-          />
-         ):(
-          <>
-          <p className="document-title">{documentTitle }</p>
-          </>
-         )}
-         {currentUserType=='editor'  && !editing &&(
-          <Image 
-          src='/assets/icons/edit.svg'
-          alt='edit'
-          width={24}
-          height={24}
-          onClick={()=> setEditing(true)}
-          className='pointer'
-          />
-         )}
-         {currentUserType!=='editor'&& !editing &&(
-          <p className="view-only-tag">View only</p>
-         )}
+          <Header>
+            <div ref={containerRef} className="flex w-fit items-center justify-center gap-2">
+              {editing && !loading ? (
+                <Input 
+                  type="text"
+                  value={documentTitle}
+                  ref={inputRef}
+                  placeholder="Enter title"
+                  onChange={(e) => setDocumentTitle(e.target.value)}
+                  onKeyDown={updateTitleHandler}
+                  disable={!editing}
+                  className="document-title-input"
+                />
+              ) : (
+                <>
+                  <p className="document-title">{documentTitle}</p>
+                </>
+              )}
 
-         {loading && <p className='text-sm text-gray-400'>saving...</p>}
+              {currentUserType === 'editor' && !editing && (
+                <Image 
+                  src="/assets/icons/edit.svg"
+                  alt="edit"
+                  width={24}
+                  height={24}
+                  onClick={() => setEditing(true)}
+                  className="pointer"
+                />
+              )}
+
+              {currentUserType !== 'editor' && !editing && (
+                <p className="view-only-tag">View only</p>
+              )}
+
+              {loading && <p className="text-sm text-gray-400">saving...</p>}
+            </div>
+            <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
+              <ActiveCollaborators />
+
+              <ShareModal 
+                roomId={roomId}
+                collaborators={users}
+                creatorId={roomMetadata.creatorId}
+                currentUserType={currentUserType}
+              />
+
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
+          </Header>
+        <Editor roomId={roomId} currentUserType={currentUserType} />
         </div>
-        <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
-          <ActiveCollaborators />
-          <ShareModel roomId={roomId} collaborators ={users} creatorId ={roomMetadata.creatorId}
-          currentUserType ={currentUserType}
-          />
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-        </div>
-      </Header>
-      <Editor roomId={roomId} currentUserType={currentUserType} />
-        </div>
-             </ClientSideSuspense>
-   </RoomProvider>
-   </div>
-   )
- }
+      </ClientSideSuspense>
+    </RoomProvider>
+  )
+}
 
- 
- export default CollaborativeRoom
-
-
-
-
-
-
-
-
-
-
-
+export default CollaborativeRoom
